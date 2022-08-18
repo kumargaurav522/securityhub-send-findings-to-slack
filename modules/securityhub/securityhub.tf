72d9cf44cb6696c710ee03385841da96e5e1e5ab
Gaurav <kumargaurav522@gmail.com>
2022-08-18 20:32:09 +0530

    first commit

--- modules/securityhub/securityhub.tf
+++ modules/securityhub/securityhub.tf
@@ -1,22 +1,22 @@
 
-resource "aws_securityhub_account" "default" {
-}
+# resource "aws_securityhub_account" "default" {
+# }
 
 resource "aws_securityhub_standards_subscription" "pci" {
   count         = var.subscription_pci ? 1 : 0
-  depends_on    = [aws_securityhub_account.default]
+  # depends_on    = [aws_securityhub_account.default]
   standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/pci-dss/v/3.2.1"
 }
 
 resource "aws_securityhub_standards_subscription" "cis" {
   count         = var.subscription_cis ? 1 : 0
-  depends_on    = [aws_securityhub_account.default]
+  # depends_on    = [aws_securityhub_account.default]
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 }
 
 resource "aws_securityhub_standards_subscription" "foundational" {
   count         = var.subscription_foundational ? 1 : 0
-  depends_on    = [aws_securityhub_account.default]
+  # depends_on    = [aws_securityhub_account.default]
   standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/aws-foundational-security-best-practices/v/1.0.0"
 }
 
